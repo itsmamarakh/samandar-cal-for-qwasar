c3394d2fdba87266c1debf14e8f06116e07f982a
Bobur Ibrakhimov <113016201+itsmamarkh@users.noreply.github.com>
2023-01-25 18:49:05 +0500

    Finished

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+import "./App.css";
+
+function App() {
+
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+
+    if(
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1)
+      )
+    ){
+      return;
+    }
+
+     setCalc(calc + value);
+
+     if(!ops.includes(value)){
+      setResult(eval(calc + value).toString())
+     }
+  }
+   
+   const createDigitls = () => {
+    const digitls = []
+
+    for(let i = 1; i < 10; i++){
+        digitls.push(
+          <button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>
+        )
+    }
+
+    return digitls;
+
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  const deleteLast = () =>{
+    if(calc == ''){
+      return
+    }
+
+    const value = calc.slice(0, -1)
+
+    setCalc(value)
+  }
+
+  return (
+   <div className='App'>
+      <div className='calculator'>
+
+        <div className='display'>
+          {/* {result ? <span>({result})</span> : ''} */}
+            {calc}
+        </div>
+
+         <div className='digitls'>
+         <button onClick={() => updateCalc('1')}>1</button>
+         <button onClick={() => updateCalc('2')}>2</button>
+         <button onClick={() => updateCalc('3')}>3</button>
+         <button className='operators' onClick={() => updateCalc('+')}>+</button>
+
+         <button onClick={() => updateCalc('4')}>4</button>
+         <button onClick={() => updateCalc('5')}>5</button>
+         <button onClick={() => updateCalc('6')}>6</button>
+         <button className='operators' onClick={() => updateCalc('-')}>-</button>
+
+         <button onClick={() => updateCalc('7')}>7</button>
+         <button onClick={() => updateCalc('8')}>8</button>
+         <button onClick={() => updateCalc('9')}>9</button>
+         <button className='operators' onClick={() => updateCalc('*')}>*</button>
+
+         <button className='deleteBtn' onClick={() => deleteLast('C')}>C</button>
+         <button onClick={() => updateCalc('0')}>0</button>
+         <button onClick={calculate}>=</button>
+         <button className='operators' onClick={() => updateCalc('/')}>/</button>
+         </div>
+
+      </div>
+   </div>
+  );
+}
+
+
+export default App;
